4285631767f6711240a6432cd479810d02d4f777
Sebastian Ruiz <sebasrhrh@gmail.com>
2025-04-07 22:29:32 -0500

    Minor change css style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-    <main className="bg-gray-900 h-screen p-10">
+    <main className="bg-gray-900 min-h-screen p-10">
       <div className="container mx-auto">
         <h1 className="text-5xl font-extrabold text-center text-white mb-10">
           ToDo List 📝
